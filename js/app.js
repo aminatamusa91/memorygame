ef6cb550b1c422a7389c35f1a0361f79f8e230c7
Aminata Musa <aminatamusa91@gmail.com>
2018-09-26 12:09:11 -0400

    create list that holds cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-
+var gameCards = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bomb", "fa fa-bomb", "fa fa-bicycle", "fa fa-bicycle"];
 
 /*
  * Display the cards on the page
